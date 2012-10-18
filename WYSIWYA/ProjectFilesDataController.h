c0869bb2b2744105b3a6bdd343b032e003c9dfe0
Fabian Henckmann <fabian.henckmann@gmx.de>
2012-10-18 21:02:27 +1100

    First commit

---
+++ WYSIWYA/ProjectFilesDataController.h
@@ -0,0 +1,15 @@
+//
+//  ProjectFilesDataController.h
+//  WYSIWYA
+//
+//  Created by Fabian Henckmann on 31/07/12.
+//  Copyright (c) 2012 Fabian Henckmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProjectFilesDataController : NSObject
+
+@property (nonatomic,readwrite) NSMutableArray* projectFiles;
+
+@end
---
+++ WYSIWYA/ProjectFilesDataController.m
@@ -0,0 +1,13 @@
+//
+//  ProjectFilesDataController.m
+//  WYSIWYA
+//
+//  Created by Fabian Henckmann on 31/07/12.
+//  Copyright (c) 2012 Fabian Henckmann. All rights reserved.
+//
+
+#import "ProjectFilesDataController.h"
+
+@implementation ProjectFilesDataController
+
+@end
